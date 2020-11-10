bdf8f5b77e437b76cb64ede2fd7ca8fe552c1ce1
Sergey Butyanov <igor@tomatstudio.ru>
2020-11-10 15:05:07 +0300

    show more partners

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
-const acBtns = document.querySelectorAll(".accordion__btn");
+const acBtns = document.querySelectorAll('.accordion__btn');
+const showMore = document.querySelector('.showMore');
 
 function accordionHandler() {
-    this.classList.toggle("active");
+    this.classList.toggle('active');
     let panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = panel.scrollHeight + 'px';
     }
 }
 
 acBtns.forEach(function(btn) {
-	btn.addEventListener("click", accordionHandler)
+	btn.addEventListener('click', accordionHandler)
+});
+
+showMore.addEventListener('click', function() {
+    this.previousElementSibling.style.maxHeight = 100 + 'px';
 });
