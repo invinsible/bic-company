19700265bf914f5dce1bdca2ff55e26979ed97c4
Sergey Butyanov <igor@tomatstudio.ru>
2020-11-10 15:31:25 +0300

    refactor js for IE

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 const acBtns = document.querySelectorAll('.accordion__btn');
 const showMore = document.querySelector('.showMore');
 
-function accordionHandler() {
-    this.classList.toggle('active');
-    let panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + 'px';
-    }
-}
-
-acBtns.forEach(function(btn) {
-	btn.addEventListener('click', accordionHandler)
-});
-
 showMore.addEventListener('click', function() {
-    this.previousElementSibling.style.maxHeight = 100 + 'px';
+    this.previousElementSibling.style.maxHeight = 100 + '%';
+    this.style.display = 'none';
+    this.disabled = true;
 });
+
+
+for (let i = 0; i < acBtns.length; i++) {
+    acBtns[i].addEventListener('click', function(){
+        this.classList.toggle('active');
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+    });
+}
