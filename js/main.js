8eafa477b07c5263f599ab506aa9187999bb299f
Sergey Butyanov <igor@tomatstudio.ru>
2020-11-16 12:50:15 +0300

    yandex maps

--- js/main.js
+++ js/main.js
@@ -45,3 +45,80 @@ $(document).ready(function (){
 });
 
 
+
+
+ymaps.ready(function(){
+
+    let myMap = new ymaps.Map("map", {
+        center: [59.96794514770856,30.320952949501976],
+        zoom: 10,
+        controls: ['zoomControl']
+    });   
+    
+    let coords = [
+        {
+            cd: [ 59.905295421119,30.270380973816 ],
+            title: 'БИК-Информ Главный офис',
+            adress: 'ул. Бумажная, д. 9, корп. 1, литер А',
+            mail: 'bic@bic-inform.ru',
+            site: 'bic-inform.ru',
+            phone: '+7 (812) 447-95-55'
+        },
+        {
+            cd: [ 59.905295421119,30.270380973816 ],
+            title: 'БИК Торговый Дом Главный офис',
+            adress: 'ул. Бумажная, д. 9, корп. 1, литер А',
+            mail: 'opt@bic-video.ru',
+            site: 'bic-video.ru',
+            phone: '+7 (812) 447-95-57'
+        },
+        {
+            cd: [ 59.90198268955941,30.274680805557256 ],
+            title: 'БИК Торговый Дом офис м. Нарвская',
+            adress: 'Старо-Петергофский пр., д. 43/45',
+            mail: 'info@bic-video.ru',            
+            phone: '+7 (812) 740-11-76'
+        },
+        {
+            cd: [ 60.037009867311,30.331127643585 ],
+            title: 'БИК Торговый Дом офис м. Озерки',
+            adress: 'ул. Есенина, д. 5, секция 9А',
+            mail: 'ozerki@bic-video.ru',            
+            phone: '+7 (981) 878-30-28'
+        },
+        {
+            cd: [ 59.930787,30.436817 ],
+            title: 'БИК Торговый Дом офис м. Ладожская',
+            adress: 'пр. Энергетиков, д. 3, лит. Б, секция 32',
+            mail: 'lad@bic-video.ru',            
+            phone: '+7 (911) 709-35-29'
+        },
+        {
+            cd: [ 59.903986507823696,30.27436528836057 ],
+            title: 'БИК Сервис Центр',
+            adress: 'Нарвский пр., д. 14',
+            mail: 'service@bic-video.ru',            
+            phone: '+7 (812) 747-32-66'
+        }
+      ];
+
+      let myCollection = new ymaps.GeoObjectCollection({},{
+        iconLayout: 'default#image',
+        iconImageHref: '/img/marker.png',
+        iconImageSize: [41, 58],
+        iconImageOffset: [0, -58],
+    });
+
+    for (let i = 0; i < coords.length; i++) {
+    myCollection.add(
+        new ymaps.Placemark(coords[i].cd, {
+        balloonContentHeader: coords[i].title,
+        balloonContentBody: coords[i].phone + '<br>' + coords[i].adress + '<br>' + '<a href="mailto:'+ coords[i].mail +'" target="_blank">'+ coords[i].mail + '</a>',
+        balloonContentFooter: '<a href="tel:'+ coords[i].phone + '">'+ coords[i].phone + '</a>'
+        })
+    );
+    }
+      
+    myMap.geoObjects.add(myCollection);  
+  
+});
